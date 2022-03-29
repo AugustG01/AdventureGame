7ba7b823d0cc76f4130181eb9e447fb87c8d5a97
AugustG01 <99917848+AugustG01@users.noreply.github.com>
2022-03-29 15:10:59 +0200

    Update MeleeWeapon.java

--- src/MeleeWeapon.java
+++ src/MeleeWeapon.java
@@ -1,7 +1,9 @@
+import java.util.Random;
+
 public class MeleeWeapon extends Weapon{
 
-    MeleeWeapon(String description, int damage) {
-        super(description, damage);
+    MeleeWeapon(String description, int damage, int criticalChance, int missChance) {
+        super(description, damage, criticalChance, missChance);
     }
 
 }
