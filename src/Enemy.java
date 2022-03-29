40fc8fb6cae588e913c06e0e85e4910c1d84d459
AugustG01 <augu8204@stud.kea.dk>
2022-03-29 12:10:47 +0200

    Enemy kan

---
+++ src/Enemy.java
@@ -0,0 +1,62 @@
+public class Enemy {
+    private String name;
+    private String description;
+    private Item weapon;
+    private int health;
+
+
+    public Enemy(String name, String description, Item weapon, int health){
+        setName(name);
+        setDescription(description);
+        setWeapon(weapon);
+        setHealth(health);
+    }
+
+    public void hit(int damage) {
+        this.health -= damage;
+        System.out.println("You hit " + name + " and did " + damage + " damage to him");
+        System.out.println(name + " now has " + health + " HP");
+
+
+
+
+
+        System.out.println(damage);
+        System.out.println(health);
+    }
+
+    public int attack() {
+        return ((Weapon)weapon).attack();
+    }
+
+    public Item drop() {
+        return weapon;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public Item getWeapon(){
+        return weapon;
+    }
+    public int getHealth(){
+        return health;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setWeapon(Item weapon){
+        this.weapon = weapon;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
