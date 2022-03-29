bd5b46efa9cfc3f3c79dfc9a1c29e184a6ccfff2
AugustG01 <99917848+AugustG01@users.noreply.github.com>
2022-03-29 15:06:37 +0200

    Update Weapon.java

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,13 +1,48 @@
+import java.util.Random;
+
 public abstract class Weapon extends Item{
     private int damage;
-    Weapon(String description, int damage){
+    private int criticalChance;
+    private int missChance;
+    Random random = new Random();
+
+    Weapon(String description, int damage, int criticalChance, int missChance){
         super(description);
         this.damage = damage;
+        this.criticalChance = criticalChance;
+        this.missChance = missChance;
     }
     public void setDamage(int damage){
         this.damage = damage;
     }
     public int getDamage() {
+        return damage;
+    }
+    public int attack(){
+
+        if(missChance())
+            return 0;
+        else
+            return criticalChance();
+    }
+
+    public boolean missChance() {
+        int miss = random.nextInt(missChance);
+        //System.out.println("miss random number: " + miss);
+        if (miss == 0) {
+            System.out.println("Miss");
+            return true;
+        }
+        return false;
+    }
+    public int criticalChance() {
+        int crit = random.nextInt(criticalChance);
+        //System.out.println("critical random number: " + crit);
+        if (crit == 0) {
+            System.out.println("Critical");
+            return damage * 2;
+        }
+        else
         return damage;
     }
 }
