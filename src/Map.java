97e87ad7d4a0cfca379a51e1e7f923ff7162b285
AugustG01 <99917848+AugustG01@users.noreply.github.com>
2022-03-30 10:39:15 +0200

    Update Map.java

--- src/Map.java
+++ src/Map.java
@@ -3,71 +3,85 @@ public class Map {
 
     //create Items
 
-    Item corpse = new Item("corpse");
-    Item chair = new Item("chair");
-    Item anvil = new Item("anvil");
-    Item lamp = new Item("lamp");
-    Item cheeseGrater = new Item("cheese grater");
-
-    Item candle = new Item("candle");
-
-
-
+    Item corpse = new Item("Corpse");
+    Item chair = new Item("Chair");
+    Item anvil = new Item("Anvil");
+    Item lamp = new Item("Lamp");
+    Item cheeseGrater = new Item("Cheese grater");
+    Item magicBeans = new Item("Magic beans");
+    Item dress = new Item("A red dress");
+    Item candle = new Item("Candle");
+    Item fireExtinguisher = new Item("Fire extinguisher");
+    Item carpet = new Item("Carpet");
 
 
     // create weapons
     // melee
-    Item axe = new MeleeWeapon("axe", 10, 10, 10);
-    Item knife = new MeleeWeapon("knife", 5, 10, 10);
-    Item shovel = new MeleeWeapon("shovel", 3, 6, 5);
+    Item axe = new MeleeWeapon("Axe", 10, 50, 8);
+    Item knife = new MeleeWeapon("Knife", 5, 10, 10);
+    Item shovel = new MeleeWeapon("Shovel", 4, 4, 5);
+    Item teeth = new MeleeWeapon("Rat teeth", 3,3,1);
+    Item hoe = new MeleeWeapon("Hoe", 4, 7, 5);
+    Item plunger = new MeleeWeapon("Plunger", 2, 2, 999);
+    Item magicWand = new MeleeWeapon("Magic wand", 15, 4, 15);
 
     //ranged
-    Item bow = new RangedWeapon("bow", 3, 5, 5, 50);
-    Item throwDagger = new RangedWeapon("Throwing Dagger", 2, 5, 5, 50);
+    Item bow = new RangedWeapon("Bow", 12, 5, 2, 10);
+    Item throwDagger = new RangedWeapon("Throwing Dagger", 7, 5, 6, 5);
+    Item bazooka = new RangedWeapon("Bazooka", 50, 50, 1, 3);
 
 
     // create food
     //unhealthy
-    Item rottenBeef = new Food("rotten beef", -25);
+    Item rottenBeef = new Food("Rotten beef", -25);
+    Item poisonedCake = new Food("Cake", -60);
+    Item rottenApple = new Food("Apple", -30);
+    Item bluePills = new Food("Blue pills", -20);
 
     //healthy
-    Item apple = new Food("apple", 25);
+    Item apple = new Food("Apple", 25);
+    Item carrot = new Food("Carrot", 5);
+    Item bread = new Food("Bread", 5);
+    Item cucumber = new Food("Cucumber", 10);
+    Item cookedMeat = new Food("Meat", 35);
+    Item redPills = new Food("Red pills", 40);
 
 
     // create enemies
-    Enemy dummy = new Enemy("john", "dangerous", knife, 50);
-    Enemy zombie = new Enemy("Alfred", "the zombie", shovel, 70);
+    Enemy dummy = new Enemy("John", "The dangerous", knife, 50);
+    Enemy zombie = new Enemy("Alfred", "The zombie", shovel, 70);
+    Enemy rat = new Enemy("Rat", "The evil", teeth, 2);
+    Enemy undeadSoldier = new Enemy("Soldier with a bazooka", "Undead", bazooka, 20);
+    Enemy witch = new Enemy("Witch", " with a wooden stick", magicWand, 100);
+
 
     // create descriptions
     void gameSetup(){
         Room room1 = new Room("The Field", """
                 You stand in front of a decaying house, and spot two doors.""");
-        Room room2 = new Room("Main Entrance","""
-            You entered the main entrance, there is broken glass all over the ground,
+        Room room2 = new Room("The Main Entrance","""
+            There is broken glass all over the ground,
             among the shattered glass, are broken picture frames""");
-        Room room3 = new Room("Dining Room","""
-                You entered the dining room, there is a big old clock which is very loud,
+        Room room3 = new Room("The Dining Room","""
+                There is a big old clock which is very loud,
                 dining table has mold growing on it""");
-        Room room4 = new Room("Back entrance", """
+        Room room4 = new Room("The Back entrance", """
                 You are standing on a rotten carpet.
                 A bit of light enters through a broken window.
                 A sweet smell of decay comes from the the southern door""");
         Room room5 = new Room("The dungeon", """
-                All dark. You have no sense of direction.
-                Some heavy creature is breathing nearby. It seems to be coming your way.""");
+                All dark. You have no sense of direction.""");
         Room room6 = new Room("The Lounge", """
                 There are no windows in this room.
                 The only light is a small candlelight in the southeastern corner.
                 A door to the south is ajar.""");
-        Room room7 = new Room("Bedroom","""
-                The room seems to be a bedroom but it's very dark.
+        Room room7 = new Room("The Bedroom","""
+                It's very dark and hard to see.
                 You can feel cobweb all over you and see a little reflection near the night stand.""");
-        Room room8 = new Room("Kitchen","""
-                You are in what seems to be the kitchen.
+        Room room8 = new Room("The Kitchen","""
                 It's very smokey and hard to see.
                 You can faintly see a staircase but can't put a finger on the which way to get to it.""");
-        Room room9 = new Room("Bathroom","""
-                Room 9 seems to be the bathroom.
+        Room room9 = new Room("The Bathroom","""
                 It's very cold since a small window is broken and banging against the outside bricks.
                 You see the medicine closet is slightly open.""");
 
@@ -92,16 +106,21 @@ public class Map {
         room4.setNorth(room1);
 
         // place items in map
-        room1.itemsInRoom.setItem(chair, axe, rottenBeef);
         room2.itemsInRoom.setItem(cheeseGrater, axe);
-        room3.itemsInRoom.setItem(knife, lamp, apple);
-        room4.itemsInRoom.setItem(bow, candle, throwDagger);
-        room5.itemsInRoom.setItem(anvil, corpse);
+        room3.itemsInRoom.setItem(lamp, apple, cookedMeat);
+        room4.itemsInRoom.setItem(bow, candle, bread);
+        room5.itemsInRoom.setItem(anvil, corpse, throwDagger);
+        room6.itemsInRoom.setItem(chair, rottenBeef, rottenApple);
+        room7.itemsInRoom.setItem(dress, cucumber, carpet);
+        room8.itemsInRoom.setItem(knife, fireExtinguisher, poisonedCake);
+        room9.itemsInRoom.setItem(bluePills, redPills, plunger);
 
         // place enemies
-        room1.setEnemy(dummy);
-        room2.setEnemy(zombie);
-
+        room5.setEnemy(dummy);
+        room3.setEnemy(rat);
+        room4.setEnemy(zombie);
+        room7.setEnemy(witch);
+        room6.setEnemy(undeadSoldier);
 
         startRoom = room1;
     }
